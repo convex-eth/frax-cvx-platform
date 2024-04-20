6d285658b4c955ea0d4d12481f3470c7e69c24a8
C2tP <c2tp@protonmail.com>
2024-04-20 22:30:21 +0900

    fpis/cvxfpis deposit for cvxfxs

---
+++ contracts/contracts/fraxtal/FpisMigrate.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "../interfaces/ITokenMinter.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract FpisMigrate is ReentrancyGuard{
+    using SafeERC20 for IERC20;
+
+    address public owner;
+    address public pendingOwner;
+
+    address public immutable fpis;
+    address public immutable cvxfpis;
+    address public immutable cvxfxs;
+    uint256 public immutable rate;
+    uint256 constant denominator = 1e18;
+    
+    event SetPendingOwner(address indexed _address);
+    event OwnerChanged(address indexed _address);
+    event WithdrawTo(address indexed _address, uint256 _amount);
+    event Migrated(address indexed _address, uint256 _fpis, uint256 _cvxfpis, uint256 _cvxfxs);
+
+    constructor(address _fpis, address _cvxfpis, address _cvxfxs, uint256 _rate){
+        owner = msg.sender;
+        fpis = _fpis;
+        cvxfpis = _cvxfpis;
+        cvxfxs = _cvxfxs;
+        rate = _rate;
+    }
+
+    //set next owner
+    function setPendingOwner(address _po) external {
+        require(msg.sender == owner, "!auth");
+        pendingOwner = _po;
+        emit SetPendingOwner(_po);
+    }
+
+    //claim ownership
+    function acceptPendingOwner() external {
+        require(msg.sender == pendingOwner, "!p_owner");
+
+        owner = pendingOwner;
+        pendingOwner = address(0);
+        emit OwnerChanged(owner);
+    }
+
+    function execute(
+        address _to,
+        uint256 _value,
+        bytes calldata _data
+    ) external returns (bool, bytes memory) {
+        require(msg.sender == owner,"!auth");
+
+        (bool success, bytes memory result) = _to.call{value:_value}(_data);
+
+        return (success, result);
+    }
+
+    function withdrawTo(IERC20 _asset, uint256 _amount, address _to) external {
+        require(msg.sender == owner, "!auth");
+
+        _asset.safeTransfer(_to, _amount);
+        emit WithdrawTo(_to, _amount);
+    }
+
+    function migrate(uint256 _fpisAmount, uint256 _cvxfpisAmount) external nonReentrant{
+        //pull fpis
+        if(_fpisAmount > 0){
+            IERC20(fpis).safeTransferFrom(msg.sender, address(this), _fpisAmount);
+        }
+
+        //pull cvxfpis
+        if(_cvxfpisAmount > 0){
+            IERC20(cvxfpis).safeTransferFrom(msg.sender, address(this), _cvxfpisAmount);
+        }
+
+        //combine
+        uint256 mintAmount = _fpisAmount + _cvxfpisAmount;
+        //apply rate
+        mintAmount = mintAmount * rate / denominator;
+        require(mintAmount > 0,"!minimal amount");
+
+        //mint back cvxfxs
+        ITokenMinter(cvxfxs).mint(msg.sender, mintAmount);
+        emit Migrated(msg.sender, _fpisAmount, _cvxfpisAmount, mintAmount);
+    }
+}
---
+++ contracts/test/UI_4_fpismerge.js
@@ -0,0 +1,193 @@
+// const { BN, constants, expectEvent, expectRevert, time } = require('openzeppelin-test-helpers');
+const { BN, time } = require('openzeppelin-test-helpers');
+var jsonfile = require('jsonfile');
+var contractList = jsonfile.readFileSync('./contracts.json');
+
+const FraxtalVoterProxy = artifacts.require("FraxtalVoterProxy");
+const FraxtalBooster = artifacts.require("FraxtalBooster");
+const FraxtalFxsDepositor = artifacts.require("FraxtalFxsDepositor");
+const StakedCvxFxs = artifacts.require("StakedCvxFxs");
+const cvxToken = artifacts.require("cvxToken");
+const IFraxtalVoteEscrow = artifacts.require("IFraxtalVoteEscrow");
+const FpisMigrate = artifacts.require("FpisMigrate");
+
+const IERC20 = artifacts.require("IERC20");
+const ERC20 = artifacts.require("ERC20");
+
+
+const unlockAccount = async (address) => {
+  let NETWORK = config.network;
+  if(!NETWORK.includes("debug")){
+    return null;
+  }
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "hardhat_impersonateAccount",
+        params: [address],
+        id: new Date().getTime(),
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
+
+const setNoGas = async () => {
+  let NETWORK = config.network;
+  if(!NETWORK.includes("debug")){
+    return null;
+  }
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(
+      {
+        jsonrpc: "2.0",
+        method: "hardhat_setNextBlockBaseFeePerGas",
+        params: ["0x0"],
+        id: new Date().getTime(),
+      },
+      (err, result) => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve(result);
+      }
+    );
+  });
+};
+
+const send = payload => {
+  if (!payload.jsonrpc) payload.jsonrpc = '2.0';
+  if (!payload.id) payload.id = new Date().getTime();
+
+  return new Promise((resolve, reject) => {
+    web3.currentProvider.send(payload, (error, result) => {
+      if (error) return reject(error);
+
+      return resolve(result);
+    });
+  });
+};
+
+/**
+ *  Mines a single block in Ganache (evm_mine is non-standard)
+ */
+const mineBlock = () => send({ method: 'evm_mine' });
+
+/**
+ *  Gets the time of the last block.
+ */
+const currentTime = async () => {
+  const { timestamp } = await web3.eth.getBlock('latest');
+  return timestamp;
+};
+
+/**
+ *  Increases the time in the EVM.
+ *  @param seconds Number of seconds to increase the time by
+ */
+const fastForward = async seconds => {
+  // It's handy to be able to be able to pass big numbers in as we can just
+  // query them from the contract, then send them back. If not changed to
+  // a number, this causes much larger fast forwards than expected without error.
+  if (BN.isBN(seconds)) seconds = seconds.toNumber();
+
+  // And same with strings.
+  if (typeof seconds === 'string') seconds = parseFloat(seconds);
+
+  await send({
+    method: 'evm_increaseTime',
+    params: [seconds],
+  });
+
+  await mineBlock();
+};
+
+const getChainContracts = () => {
+  let NETWORK = config.network;//process.env.NETWORK;
+  console.log("network: " +NETWORK);
+  var contracts = {};
+
+  if(NETWORK == "debugFraxtal" || NETWORK == "mainnetFraxtal"){
+    contracts = contractList.fraxtal;
+  }
+
+  return contracts;
+}
+
+const advanceTime = async (secondsElaspse) => {
+  await fastForward(secondsElaspse);
+  console.log("\n  >>>>  advance time " +(secondsElaspse/86400) +" days  >>>>\n");
+}
+const day = 86400;
+
+contract("Deploy and test locking", async accounts => {
+  it("should deploy contracts and test various functions", async () => {
+
+    let chainContracts = getChainContracts();
+    let deployer = chainContracts.system.deployer;
+    let multisig = chainContracts.system.multisig;
+    let addressZero = "0x0000000000000000000000000000000000000000"
+
+    let userA = accounts[0];
+    let userB = accounts[1];
+    let userC = accounts[2];
+    let userD = accounts[3];
+    let userZ = "0xAAc0aa431c237C2C0B5f041c8e59B3f1a43aC78F";
+    var userNames = {};
+    userNames[userA] = "A";
+    userNames[userB] = "B";
+    userNames[userC] = "C";
+    userNames[userD] = "D";
+    userNames[userZ] = "Z";
+
+    
+    await unlockAccount(deployer);
+
+    var voteproxy = await FraxtalVoterProxy.at(chainContracts.system.voteProxy);
+    var booster = await FraxtalBooster.at(chainContracts.system.booster);
+    var fpisBooster = await FraxtalBooster.at(chainContracts.system.fpisBooster);
+    var fxs = await IERC20.at(chainContracts.frax.fxs);
+    var fpis = await IERC20.at(chainContracts.frax.fpis);
+    var cvxfxs = await cvxToken.at(chainContracts.system.cvxFxs);
+    var cvxfpis = await cvxToken.at(chainContracts.system.cvxFpis);
+    var vefxs = await IFraxtalVoteEscrow.at(chainContracts.frax.vefxs);
+
+    //deploy
+    var migrate = await FpisMigrate.new(chainContracts.frax.fpis, chainContracts.system.cvxFpis, chainContracts.system.cvxFxs, "33333333333333333", {from:deployer});
+    console.log("migrator: " +migrate.address);
+
+    //set mint role on migrate
+    var minterdata = cvxfxs.contract.methods.setOperator(migrate.address,true).encodeABI();
+    // console.log(minterdata);
+
+    await booster.execute(cvxfxs.address, minterdata, {from:deployer});
+    // console.log("migrate given mint role of cvxfxs")
+    // await cvxfxs.operators(migrate.address).then(a=>console.log("migrator is operator of cvxfxs? " +a));
+
+    
+    //get more fpis
+    var holder = "0xcD3A040f05769d7628582B403063e61B7D212F91";
+    await unlockAccount(holder);
+    await setNoGas();
+    await fpis.transfer(userA, web3.utils.toWei("1000.0", "ether"),{from:holder,gasPrice:0})
+    await fpis.balanceOf(userA).then(a=>console.log("transfered fpis: " +a))
+    await setNoGas();
+    var minterdata = cvxfxs.contract.methods.setOperator(deployer,true).encodeABI();
+    await fpisBooster.execute(cvxfpis.address, minterdata, {from:deployer});
+    // console.log("deployer given mint role of cvxfpis")
+    // await cvxfpis.operators(deployer).then(a=>console.log("deployer is operator of cvxfpis? " +a));
+    await cvxfpis.mint(userA, web3.utils.toWei("1000.0", "ether"), {from:deployer,gasPrice:0})
+    await cvxfpis.balanceOf(userA).then(a=>console.log("transfered cvxfpis: " +a))
+
+    return;
+
+  });
+});
+
+
